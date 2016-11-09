0efec04d906ef13a8ca1c9f76f3dd3f19581e61d
CrazyMax <crazy-max@users.noreply.github.com>
2016-11-09 02:07:58 +0100

    Add bundles : 2.7.12.3, 3.4.4.4, 3.5.2.2

---
+++ bin/python2.7.12.3/bin/python.bat
@@ -0,0 +1,6 @@
+@echo off
+set WINPYSCRIPTSDIR=%~dp0..\scripts
+call "%WINPYSCRIPTSDIR%\env_for_icons.bat"
+cd/D "%WINPYWORKDIR%"
+rem backward compatibility for  python command-line users
+"%WINPYDIR%\python.exe"  %*
