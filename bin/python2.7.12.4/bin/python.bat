b416b1930f316c76c69ea279a7882c985768bd94
CrazyMax <crazy-max@users.noreply.github.com>
2017-03-02 06:14:55 +0100

    Typo

--- bin/python2.7.12.4/bin/python.bat
+++ bin/python2.7.12.4/bin/python.bat
@@ -2,5 +2,5 @@
 set WINPYSCRIPTSDIR=%~dp0..\scripts
 call "%WINPYSCRIPTSDIR%\env_for_icons.bat"
 cd/D "%WINPYWORKDIR%"
-rem backward compatibility for  python command-line users
-"%WINPYDIR%\python.exe"  %*
+rem backward compatibility for python command-line users
+"%WINPYDIR%\python.exe" %*
