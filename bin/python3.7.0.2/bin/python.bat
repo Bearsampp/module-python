e7532d64117b0a39f575a7b73205836304b3d476
Bear <programming@hallhome.us>
2022-09-07 10:52:32 -0500

    pause point

--- bin/python3.7.0.2/bin/python.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-set WINPYSCRIPTSDIR=%~dp0..\scripts
-call "%WINPYSCRIPTSDIR%\env_for_icons.bat"
-cd/D "%WINPYWORKDIR%"
-rem backward compatibility for python command-line users
-"%WINPYDIR%\python.exe" %*
